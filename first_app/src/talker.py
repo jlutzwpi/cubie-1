e234ccf3f6b16774fb354c90f878120c27db9a37
Justin Lutz <justin.e.lutz@gmail.com>
2023-01-15 21:28:54 -0500

    first commit

--- first_app/src/talker.py
+++ first_app/src/talker.py
@@ -6,7 +6,7 @@ from rclpy.node import Node
 def main(args=None):
     rclpy.init(args=args)
     node = Node("py_test")
-    node.get_logger().info("Hello ROS2")
+    node.get_logger().info("Hello ROS2 - Yay!")
     rclpy.spin(node)
     rclpy.shutdown()
 
